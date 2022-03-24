cf92ed8dbea6a6052dcac81f770173ce80f96b82
acho01 <achosharashenidze@gmail.com>
2022-03-25 00:31:01 +0400

    response parsing added

--- build.sbt
+++ build.sbt
@@ -5,3 +5,4 @@ version := "0.1"
 scalaVersion := "2.13.8"
 
 libraryDependencies += "org.apache.httpcomponents" % "httpclient" % "4.5.13"
+libraryDependencies += "com.lihaoyi" %% "upickle" % "1.5.0"
--- src/main/scala/TranslatorCli.scala
+++ src/main/scala/TranslatorCli.scala
@@ -1,18 +1,27 @@
 import org.apache.http.HttpStatus
-import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.methods.{CloseableHttpResponse, HttpGet}
 import org.apache.http.impl.client.HttpClientBuilder
 import org.apache.http.util.EntityUtils
 
 object TranslatorCli extends App {
+
+  def parse(response: CloseableHttpResponse): String = {
+    val entity = EntityUtils.toString(response.getEntity)
+    val json = ujson.read(entity)
+    json("rows").arr
+      .map(item => item("value"))
+      .map(item => (item("Word"), item("Text")))
+      .map(tuple => tuple._1 + " - " + tuple._2 + "\n")
+      .reduceLeft(_ + _)
+  }
 
   def handleGetRequest = {
     val httpGet = new HttpGet(s"$url$word")
     val client = HttpClientBuilder.create().build()
     val response = client.execute(httpGet)
     response.getStatusLine.getStatusCode match {
-      case HttpStatus.SC_OK => {
-        println(EntityUtils.toString(response.getEntity))
-      }
+      case HttpStatus.SC_OK =>
+        println(parse(response))
     }
   }
 
@@ -24,5 +33,4 @@ object TranslatorCli extends App {
   command match {
     case "get" => handleGetRequest
   }
-
 }
